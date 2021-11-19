65461e0506814aaccf38e1767a765c8fe1ccf123
alexh0810 <baohoan081097@gmail.com>
2021-11-19 17:16:00 +0200

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/main/java/com/example/parcelnerd/domain/ParcelRepository.java

--- src/main/java/com/example/parcelnerd/ParcelnerdApplication.java
+++ src/main/java/com/example/parcelnerd/ParcelnerdApplication.java
@@ -22,21 +22,6 @@ public class ParcelnerdApplication {
     @Bean
     public CommandLineRunner packageDemo(ParcelRepository repository, UserRepository urepository) {
         return (args) -> {
-            log.info("Save a couple of packages");
-            repository.save(new Parcel("Apple iPhone 13 Pro Max 1TB", LocalDate.of(2021, 10, 13), "Pakkamestarinkatu 1D 52, 00500, Helsinki", "James Wand", Status.SHIPPED));
-            repository.save(new Parcel("Sony DualSense Controller White PS5", LocalDate.of(2021, 10, 21), "Hätilänkatu 54, 15610, Lahti", "Ari Paatelainen", Status.SHIPPING));
-            repository.save(new Parcel("Samsung 4K Ultra HD LED TV", LocalDate.of(2021, 10, 15), "Akonmäentie 114, 73900, Rautavaara", "Kimi Viinikka", Status.SHIPPING));
-            repository.save(new Parcel("Apple AirPods Pro earphones", LocalDate.of(2021, 10, 17), "Kerkkolankatu 48, 21380, Aura", "Impi Hasti", Status.SHIPPED));
-
-            User user1 = new User("user", "$2a$06$3jYRJrg0ghaaypjZ/.g4SethoeA51ph3UD4kZi9oPkeMTpjKU5uo6", "USER");
-            User user2 = new User("admin", "$2a$10$0MMwY.IQqpsVc1jC8u7IJ.2rT8b0Cd3b3sfIBGV2zfgnPGtT4r0.C", "ADMIN");
-            urepository.save(user1);
-            urepository.save(user2);
-
-            log.info("Fetch all packages");
-            for (Parcel parcel : repository.findAll()) {
-                log.info(parcel.toString());
-            }
         };
     }
 }
